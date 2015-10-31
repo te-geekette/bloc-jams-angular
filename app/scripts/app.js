07cf59cb21ef142b8c35c053504f9d0df2536c48
Tina Egolf <tinaegolf@gmail.com>
2015-10-31 16:05:40 +0100

    Injected UI-Route Module

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,19 @@
-var blocJamsModule = angular.module('blocJams', []);
+var blocJamsModule = angular.module('blocJams', ['ui.router']);
+
+// configuring the providers we'll need. In this case for state behaviour und URL handling
+blocJamsModule.config(function($stateProvider, $locationProvider){
+    
+    // standard config, makes sure the URL is shown without a hashbang #!
+    $locationProvider.html5Mode({
+        enable: true,
+        requireBase: false,
+    });
+    
+    // defining the necessary states and their aspects 
+    $stateProvider.state('album', {
+        url: '/album',
+        controller: 'Album.controller',
+        templateUrl: '/templates/album.html'
+    
+    });
+});
